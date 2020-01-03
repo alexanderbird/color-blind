28f078ff57f08e7020929f51441cae2d5114d6a7
Alexander Bird <alex@alexanderbird.software>
2020-01-03 16:50:35 -0700

    Add Jest for unit testing

---
+++ test/Color.spec.js
@@ -0,0 +1,9 @@
+import { Color } from '../src/Color';
+
+describe('Color', () => {
+  describe('.fromHexString', () => {
+    it('returns a Color object matching that hex code', () => {
+      expect(true).toBe(true);
+    });
+  });
+});
