24dd6f38c505bfe74037e2e029cbd4aec9381c14
Alexander Bird <alex@alexanderbird.software>
2020-01-07 16:54:31 -0700

    Introduce App component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import './style';
 import { Component } from 'preact';
+import { App } from './looks/App';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello, World!</h1>
-      </div>
+      <App />
     );
   }
 }
---
+++ src/looks/App.js
@@ -0,0 +1,5 @@
+export const App = () => (
+  <div>
+    <h1>Hello, World!</h1>
+  </div>
+)
